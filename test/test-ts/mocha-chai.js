fa17ca214d22d0e819820d0fa6f17b4a092adc0e
soraping <soraping@163.com>
2017-04-21 17:10:44 +0800

    test

--- test/test-ts/mocha-chai.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const chai = require("chai");
-const sinonChai = require("sinon-chai");
-chai.use(sinonChai);
-chai.config.includeStack = true;
-exports.default = chai.expect;
